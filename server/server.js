bbfc2af3192b890ad814720a6ac77dca4fb949f6
Kuldeep Suthar <sutharkuldeep458@gmail.com>
2023-02-04 18:42:53 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,43 @@
+ import express from 'express';
+ import cors from 'cors';
+ import morgan from 'morgan';
+ import connect from './database/conn.js';
+ import router from './router/route.js';
+
+ const app = express();
+
+ /****** middleware */
+
+ app.use(express.json());
+ app.use(cors());
+ app.use(morgan('tiny'));
+ app.disable('x-powered-by'); // less hacker know about our stack
+
+ const port = 8080;
+
+ /** HTTP Get request  **/
+
+ app.get('/', (req,res) =>{
+      res.status(201).json("Home Get Request")
+ } );
+
+
+ /**** api routes */
+ app.use('/api', router)
+
+ /*******   start server only when we have valid connection */
+ 
+ connect().then(()=>{
+    try {
+        app.listen(port , ()=>{
+            console.log(`Server connected to http://localhost:${port}`);
+         })
+    } catch (error) {
+        console.log('cannot connected to server')
+    }
+ }).catch(error =>{
+    console.log("Invalid database connection ...!")
+ })
+
+
+
